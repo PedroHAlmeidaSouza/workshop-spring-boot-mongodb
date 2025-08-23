b533672845af414ef1d5937f654d7e36e390dbbe
Pedro Henrique <ph8584774@gmail.com>
2025-08-23 12:20:05 -0300

    Consulta utilizando @Query

--- Workshop/src/main/java/com/workshop_spring_boot_mongodb/workshop/repositories/PostRepository.java
+++ Workshop/src/main/java/com/workshop_spring_boot_mongodb/workshop/repositories/PostRepository.java
@@ -2,10 +2,14 @@ package com.workshop_spring_boot_mongodb.workshop.repositories;
 
 import com.workshop_spring_boot_mongodb.workshop.entities.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 
     List<Post> findByTitleContainingIgnoreCase(String title);
+
+    @Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle(String title);
 }
